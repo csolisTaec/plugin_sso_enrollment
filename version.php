68b4059d90bc151b9f4210abe2efe7465a8a21c8
csolisTaec <51968186+csolisTaec@users.noreply.github.com>
2026-06-05 14:28:17 -0600

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'auth_sso_custom';
+$plugin->version   = 2026042200;
+$plugin->requires  = 2020110900;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = 'v0.1';
